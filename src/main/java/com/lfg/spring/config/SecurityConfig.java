dc33176b3b7c4cec893b9245a61648311262f954
WaiLin <leon.wai@live.com>
2020-08-15 21:32:02 +0630

    removed rabbitmq

--- src/main/java/com/lfg/spring/config/SecurityConfig.java
+++ src/main/java/com/lfg/spring/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.lfg.spring.config;
 
-//import com.lfg.spring.JWT.JWTAuthEntryPoint;
-//import com.lfg.spring.JWT.JWTFilter;
+import com.lfg.spring.JWT.JWTAuthEntryPoint;
+import com.lfg.spring.JWT.JWTFilter;
 import com.lfg.spring.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -27,11 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
 
-//    @Autowired
-//    private JWTFilter jwtFilter;
-//
-//    @Autowired
-//    private JWTAuthEntryPoint jwtAuthEntryPoint;
+    @Autowired
+    private JWTFilter jwtFilter;
+
+    @Autowired
+    private JWTAuthEntryPoint jwtAuthEntryPoint;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,17 +41,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-//                .exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).and()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-//                .antMatchers("/","/login","/register","/validation/**/**","/ws/**/**","/user/**","/reply","/app/**/**").permitAll()
-//                .antMatchers(HttpMethod.GET).permitAll()
-//                .antMatchers(HttpMethod.OPTIONS).permitAll()  //need for cors pre check
-//                .antMatchers(HttpMethod.POST).hasRole("USER")
-//                .antMatchers(HttpMethod.PUT).hasRole("USER")
-//                .antMatchers(HttpMethod.DELETE).hasRole("USER")
-                .antMatchers("/ws/**/**/**", "/app/**/**","/topic/**/**").permitAll();
-//        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                .antMatchers("/","/login","/register","/validation/**/**").permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()  //need for cors pre check
+                .antMatchers(HttpMethod.POST).hasRole("USER")
+                .antMatchers(HttpMethod.PUT).hasRole("USER")
+                .antMatchers(HttpMethod.DELETE).hasRole("USER");
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
 
