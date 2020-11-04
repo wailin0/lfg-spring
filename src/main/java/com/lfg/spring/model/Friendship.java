2548a164020bce015d0f9c869fc411c2aa3d53aa
YasserYka <yasseryka@gmail.com>
2020-11-04 18:44:15 +0300

    friendship class and enum for friendship status

--- src/main/java/com/lfg/spring/model/Friendship.java
+++ src/main/java/com/lfg/spring/model/Friendship.java
@@ -1,5 +1,35 @@
 package com.lfg.spring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import com.lfg.spring.model.enums.FriendshipStatus;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Friendship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long friendshipId;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name="userId")
+    private User friendOf;
+
+    private FriendshipStatus status;
 
 }
