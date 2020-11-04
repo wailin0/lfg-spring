022d24db4ced9cd1798ef38469a4ef178d4a779b
YasserYka <yasseryka@gmail.com>
2020-11-04 18:55:20 +0300

    auth interceptor for websocket handshake

--- src/main/java/com/lfg/spring/config/WebSocketConfig.java
+++ src/main/java/com/lfg/spring/config/WebSocketConfig.java
@@ -1,10 +1,17 @@
 package com.lfg.spring.config;
 
+import java.util.Map;
+
 import com.lfg.spring.component.webSocketHandler;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @Configuration
@@ -13,8 +20,30 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        
-        registry.addHandler(new webSocketHandler(), "/");
+
+        registry.addHandler(new webSocketHandler(), "/userid/{id}").addInterceptors(authInterceptor());
     }
 
+    // to add userId attribute in the ws session
+    @Bean
+    private HandshakeInterceptor authInterceptor() {
+        return new HandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, 
+                  WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                
+                String path = request.getURI().getPath();
+    
+                // TODO: verify if current authenticated user is equal to this id and add is Authenticated attribute in the map
+                attributes.put("userId", path.substring(path.indexOf("/") + 1));
+
+                return true;
+            }
+
+			@Override
+			public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+					WebSocketHandler wsHandler, Exception exception) { }
+
+        };
+    }
 }
