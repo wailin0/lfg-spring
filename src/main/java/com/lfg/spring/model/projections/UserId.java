54d763a65460ba90970ec8c848c0b1b970e37b07
YasserYka <yasseryka@gmail.com>
2020-11-04 19:44:36 +0300

    added projection for userid for friendship.repo and friendship repo

---
+++ src/main/java/com/lfg/spring/model/projections/UserId.java
@@ -0,0 +1,8 @@
+package com.lfg.spring.model.projections;
+
+// used for querying only username from user repository, ORMs right?
+public interface UserId {
+    
+    public Long getUserId();
+
+}
---
+++ src/main/java/com/lfg/spring/repository/FriendshipRepository.java
@@ -0,0 +1,20 @@
+package com.lfg.spring.repository;
+
+import com.lfg.spring.model.Friendship;
+import com.lfg.spring.model.projections.UserId;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+
+    // gets usernames of a user's friends
+    @Query("SELECT f.friendOf.userId FROM Friendship f WHERE f.user.username = :username AND f.friendOf.isOnline = TRUE")
+    public List<UserId> findOnlineFriendsOf(String username);
+
+}
