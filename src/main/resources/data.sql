9296f30746999b5f77bda5eae95a957d0c047064
YasserYka <yasseryka@gmail.com>
2020-11-06 12:16:43 +0300

    added data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,8 @@ INSERT INTO groups (name, topic, description, type, disabled) VALUES ('groupName
 
 INSERT INTO Users (username, email, password, enabled, roles, ACCOUNT_NON_EXPIRED, ACCOUNT_NOT_LOCKED, CREDENTIALS_NON_EXPIRED, IS_ONLINE) VALUES ('user1', 'user1@gmai.com', 'pass', true, 'ROLE_USER', true, true, true, true);
 
+INSERT INTO Users (username, email, password, enabled, roles, ACCOUNT_NON_EXPIRED, ACCOUNT_NOT_LOCKED, CREDENTIALS_NON_EXPIRED, IS_ONLINE) VALUES ('user2', 'user2@gmai.com', 'pass', true, 'ROLE_USER', true, true, true, true);
+
 INSERT INTO Post (title, body, user_id, group_id) VALUES ('postTitle1', 'postBody1', (SELECT user_id FROM Users WHERE username='user1') ,(SELECT group_id FROM Groups WHERE name='groupName1'));
 
 INSERT INTO Member (role, disabled, user_id, group_id) VALUES ('ADMIN_ROLE', false ,(SELECT user_id FROM Users WHERE username='user1'), (SELECT group_id FROM Groups WHERE name='groupName1'));
@@ -9,3 +11,5 @@ INSERT INTO Member (role, disabled, user_id, group_id) VALUES ('ADMIN_ROLE', fal
 INSERT INTO Likes (liked, user_id, post_id) VALUES (true, (SELECT user_id FROM Users WHERE username='user1'), (SELECT post_id FROM Post WHERE title='postTitle1'));
 
 INSERT INTO Comment (body, user_id, post_id) VALUES ('comentBody1', (SELECT user_id FROM Users WHERE username='user1'), (SELECT post_id FROM Post WHERE title='postTitle1'));
+
+INSERT INTO Friendship (user_id, friend_of_id) VALUES ((SELECT user_id FROM Users WHERE username='user2'), (SELECT user_id FROM Users WHERE username='user1'))
